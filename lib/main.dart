2f75c6e0242fbb89624f573c28982284e8cf1156
bob <nick1387714169@gmail.com>
2022-08-26 17:18:06 +0700

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../TapViewer.dart';
+import 'package:phpservice/page/cocopage.dart';
+import 'package:phpservice/map.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,6 +30,48 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class TapMain extends StatefulWidget {
+  const TapMain({Key? key}) : super(key: key);
+
+  @override
+  _TapMainState createState() => _TapMainState();
+}
+
+class _TapMainState extends State<TapMain> {
+  @override
+  Widget build(BuildContext context) {
+    MediaQuery.of(context).padding.top;
+    kToolbarHeight;
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            elevation: 0,
+            backgroundColor: Colors.green,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(),
+            ),
+            title: const Text('Cocoworks',
+              style: TextStyle(color: Colors.white),
+            ),
+            bottom: const TabBar(
+                tabs: [
+                  Tab(
+                    text: "สวนของฉัน",
+                  ),
+                  Tab(
+                      text: "แผนที่สวนมะพร้าว"
+                  )
+                ]
+            ),
+          ),
+          body: TabBarView(children: [Cocopage(), GetPoints()],)
+      ),
+    );
+  }
+}
+
 
 
 
--- lib/page/cocoedit.dart
+++ lib/page/cocoedit.dart
@@ -140,7 +140,7 @@ class _CocoEditState extends State<CocoEdit> {
         actions: <Widget>[
           TextButton(
               onPressed: () {
-                Navigator.of(context).pop(context);
+                Navigator.of(context).pop(false);
                 Cocoedit();
                 editBang();
               },
